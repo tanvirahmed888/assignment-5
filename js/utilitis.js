423533a6adb9e611bcb7d2dac73a205b26102739
tanvirahmed888 <tanvirrefat.it@gmail.com>
2024-09-25 07:51:43 +0600

    added history section

---
+++ js/utilitis.js
@@ -0,0 +1,6 @@
+function showSectionById (Id){
+    document.getElementById('Donate-container').classList.add('hidden');
+    document.getElementById('history-container').classList.add('hidden');
+
+    document.getElementById('Id').classList.remove('hidden');
+}

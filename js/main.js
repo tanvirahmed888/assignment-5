0b6066d479d154fc8e49e4815f87cb994bbbdd65
tanvirahmed888 <tanvirrefat.it@gmail.com>
2024-09-27 15:53:55 +0600

    add js more

--- js/main.js
+++ js/main.js
@@ -1,30 +1,82 @@
 document.getElementById('donate-btn-1')
-.addEventListener('click', function(){
-const donateInput = document.getElementById('input-donate').value;
-const inputNumber = parseFloat(donateInput);
+    .addEventListener('click', function () {
+        const donateInput = document.getElementById('input-donate').value;
+        const inputNumber = parseFloat(donateInput);
 
-const mainBalance = document.getElementById('main-balance').innerText;
-const newBalance = parseFloat(mainBalance);
+        const mainBalance = document.getElementById('main-balance').innerText;
+        const newBalance = parseFloat(mainBalance);
 
-const subBalance = document.getElementById('sub-balance').innerText;
-const newSubBalance = parseFloat(subBalance);
-console.log(inputNumber, newBalance, newSubBalance)
-
-if (inputNumber < newBalance ){
-
-const totalSubBalance = newSubBalance + inputNumber ;
-
-document.getElementById('sub-balance').innerText= totalSubBalance ;
-
-const newMainBalance = newBalance - inputNumber ;
-
-document.getElementById('main-balance').innerText = newMainBalance ;
+        const subBalance = document.getElementById('sub-balance').innerText;
+        const newSubBalance = parseFloat(subBalance);
 
 
-} else {
-    alert ('Faild to add Money')
-}
+        if (inputNumber < newBalance) {
+
+            const totalSubBalance = newSubBalance + inputNumber;
+
+            document.getElementById('sub-balance').innerText = totalSubBalance;
+
+            const newMainBalance = newBalance - inputNumber;
+
+            document.getElementById('main-balance').innerText = newMainBalance;
+
+
+
+        } else {
+            alert('Failed to donate Money')
+        }
+    })
+// card 2 start
+
+document.getElementById('donate-btn-2').addEventListener('click', function () {
+
+    const inputValue2 = document.getElementById('input-donate-2').value;
+    const inputValue2Num = parseFloat(inputValue2);
+
+
+    const balanceNumber = getbalanceTextById('sub-balance-1');
+
+    const mainBalance2 = getbalanceTextById('main-balance');
+
+    if (inputValue2Num < mainBalance2) {
+        const newSubBalance2 = inputValue2Num + balanceNumber;
+        document.getElementById('sub-balance-1').innerText = newSubBalance2;
+        const newMainBalane2 = mainBalance2 - inputValue2;
+        document.getElementById('main-balance').innerText = newMainBalane2;
+    }
+    else {
+        alert('Failed to donate Money');
+    }
 })
 
 
+// card 3 start
 
+
+document.getElementById('donate-btn-3').addEventListener('click', function () {
+
+
+
+    const inputValue3 = document.getElementById('input-donate-3').value;
+    const inputValue3Num = parseFloat(inputValue3);
+
+
+
+    const subBalanceNum = getbalanceTextById('sub-balance-2');
+
+    const mainBalance3 = getbalanceTextById('main-balance');
+
+    if (inputValue3Num <= mainBalance3) {
+        const newSubbalance3 = inputValue3Num + subBalanceNum;
+        const newMainBalance3 = mainBalance3 - inputValue3Num;
+
+        document.getElementById('sub-balance-2').innerText = newSubbalance3;
+        document.getElementById('main-balance').innerText = newMainBalance3;
+
+
+    }
+    else {
+        alert('Failed to donate Money');
+    }
+
+})
--- js/utilitis.js
+++ js/utilitis.js
@@ -1,6 +1,23 @@
-function showSectionById (Id){
+function showSectionById (id){
     document.getElementById('Donate-container').classList.add('hidden');
     document.getElementById('history-container').classList.add('hidden');
 
-    document.getElementById('Id').classList.remove('hidden');
+    document.getElementById('id').classList.remove('hidden');
+}
+
+
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).Value;
+    const inputNum = parseFloat(inputValue);
+
+    return inputNum ;
+
+}
+
+function getbalanceTextById (id){
+
+    const balanceText = document.getElementById(id).innerText;
+    const balanceNum = parseFloat(balanceText);
+
+    return balanceNum ;
 }
